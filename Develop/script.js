a4cc29168b4c331f0fe8e01041f575dacb25bf8a
Ayla Dillis <aayladillis@gmail.com>
2020-06-17 18:03:53 -0700

    setting up variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,25 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+
+let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",];
+let specialCharacters = ["!", "@", "#", "$", "%", "&", "*", "_", "+"];
+let combined = [];
+
+let displayElement = document.getElementById("#password");
+
+let password = [];
+
+
 
 // Write password to the #password input
 function writePassword() {
+  
+  var numbersPrompt = confirm(
+    'Click OK to confirm including numeric characters.'
+  );
+
+  for (var i = 0; i < combined.length; i++); {
+    Math.floor(math.random)
+  }
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -12,3 +29,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
